b0c2f2ec71f943cec422df09f7288a13fe02c82b
RenanFStein <renan_stein@hotmail.com>
2022-07-27 09:00:54 -0300

    make it better

--- core/settings.py
+++ core/settings.py
@@ -28,6 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'api-emprestimos.herokuapp.com']
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 
 # Application definition
 
@@ -45,11 +50,13 @@ INSTALLED_APPS = [
     ## Apps de Terceiros ##
     'rest_framework',
     'drf_yasg',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
