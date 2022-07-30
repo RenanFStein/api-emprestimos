5298c16cc43234edb3ed3b4d9a61fa9b1b5530a0
RenanFStein <renan_stein@hotmail.com>
2022-07-30 10:07:23 -0300

    Cors API Vercel

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'api-emprestimos.herokuapp.com']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://front-end-api-emprestimo.vercel.app/"
 ]
 
 
